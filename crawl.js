a09946227359fef377bedbafe29842dfa6210609
DanielHangan01 <danhangan@hotmail.com>
2023-11-02 19:28:09 +0100

    crawlPage fully implemented, added report possibility

--- crawl.js
+++ crawl.js
@@ -34,23 +34,49 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls
 }
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++;
+        return pages
+    }
+    if (currentURL !== baseURL) {
+        pages[normalizedCurrentURL] = 1
+    } else {
+        pages[normalizedCurrentURL] = 0
+    }
+
+    console.log(`actively crawling page: ${currentURL}`)
+
+    let htmlBody = ''
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399) {
             console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
         
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes('text/html')) {
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return
+            return pages
         }
-        console.log(await resp.text())
+        htmlBody = await resp.text()
     } catch (err) {
         console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+    return pages
 }
 
 module.exports = {
---
+++ report.js
@@ -0,0 +1,25 @@
+function printReport(pages) {
+    console.log('==========')
+    console.log('REPORT')
+    console.log('==========')
+    const sortedPages = sortPages(pages)
+    for (const sortedPage of sortedPages) {
+        const url = sortedPage[0]
+        const cnt = sortedPage[1]
+        console.log(`Found ${cnt} internal links to ${url}`)
+    }
+
+}
+
+function sortPages(pages) {
+    const pagesArray = Object.entries(pages)
+    pagesArray.sort((pageA, pageB) => {
+        return pageB[1] - pageA[1]
+    })
+    return pagesArray
+}
+
+module.exports = {
+    printReport,
+    sortPages
+}
